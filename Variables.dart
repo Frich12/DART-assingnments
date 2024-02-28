a9017a68a82213a4a3bbe26d976d977bc7566b3f
Felix Michael <richfelix330@gmail.com>
2024-02-28 20:40:57 +0300

    My First commit

---
+++ Variables.dart
@@ -0,0 +1,15 @@
+void main() {
+  // declaring variables
+  String name = "John";
+  String Address = "Nanyuki";
+  num age = 70;
+  num height = 6.3;
+  bool ismarried = true;
+
+  //printing variables value
+  print("Name is $name");
+  print("Adress is $Address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Marital status is $ismarried");
+}
