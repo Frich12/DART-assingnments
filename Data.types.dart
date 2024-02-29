2a74a797e77825232d75efd18f535d595cbf0968
Felix Michael <richfelix330@gmail.com>
2024-02-29 11:38:02 +0300

    Second commit

---
+++ Data.types.dart
@@ -0,0 +1,14 @@
+void main() {
+//Declaring values
+  String institutionname = "DEAKIN UNIVERSITY";
+  String Module = "Software engineering";
+
+  //Printing values
+  print("My institutionname is $institutionname and my module is $Module");
+
+  bool isSingle = true;
+  print("Single Status is $isSingle");
+
+  List<dynamic> mixedlist = [1, "DAU", true, 30.00, 40];
+  print(mixedlist);
+}
